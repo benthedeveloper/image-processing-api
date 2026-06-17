dd327ad7f53ec712c3fcbbfff6390ca406638d5a
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-06-16 21:35:34 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// TODO add routes
+// app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.send('Hello world :)');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
